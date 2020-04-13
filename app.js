20c6fc9f462b0e9f4ed0b6ea6cb0252db0997058
imran-shaikh-au6 <imranshaikh110120@gmail.com>
2020-04-13 21:02:33 +0530

    mongo changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose")
 mongoose
     .connect(
-        "mongodb://localhost:27017/test", {
+        "mongodb+srv://imran:imranshaikh@mongodb1-1kxlr.mongodb.net/test", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         }
